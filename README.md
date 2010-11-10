e50eaf743c983a4ed923964dc8817a2efc361740
Francisco Carriedo <fcarriedo@gmail.com>
2010-11-10 15:59:57 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# jquery.blink
+
+I had to do this implementation for an unbelievable legitimate us of the evil blink at my company.
+
+Just thought of sharing as somebody might have the same unveliebable legitimate need. =P.
+
+*(Yes, I know there is one of this already!)*
+
+# Usage
+
+The following gives you regular blinking with some reasonable defaults:
+ * No max blinks
+ * blink durations ~1 second
+
+    $('selector').blink();  // 
+
+For some more advanced uses you can use:
+
+    $('selector').blink({maxBlinks: 60, blinkDuration: 1000}, callbackWhenMaxBlinksReached); 
+
+You can specify the maximum number of blinks as well as the blink duration.
+The callback will only be executed if maxBlinks was specified and reached.
---
+++ jquery.blink.js
@@ -0,0 +1,29 @@
+/**
+ * Intention:
+ *      $('selector').blink();  // Reasonable defaults. {no max blinks, blinkDuration ~1 sec, no callback}
+ *
+ *      $('selector').blink({maxBlinks: 60, blinkDuration: 1000}, callbackWhenMaxBlinksReached); 
+ */
+
+// IMPORTANT: Not ready yet (at all)! Implementing it tonight.
+
+MAX_BLINKS = 60; // Since every blink represents 1 second ~60 seconds.
+BLINK_TIME = 1000; // (milliseconds)
+
+function go() {
+    var on = true;
+    var blinkCount = 0;
+    var intervalId = setInterval(function(){
+        if(on) {
+            $('div#title').fadeTo('slow', 0.01);
+            on = false;
+        } else {
+            $('div#title').fadeTo('slow', 1);
+            if(blinkCount++ == MAX_BLINKS) {
+                clearInterval(intervalId);
+                page.sendMessage('com.zoomsystems.common.messages.StateManagerMessage', '0', 'localhost/core');
+            }
+            on = true;
+        }
+    }, BLINK_TIME/2);
+}
